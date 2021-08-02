598de3e7146530f01210dadc9e4970a9dd41f39b
Showmik Bose <officialshowmik@gmail.com>
2021-08-02 18:57:06 +0530

    Update sampleshell.sh

--- sampleshell.sh
+++ sampleshell.sh
@@ -1,11 +1,14 @@
-#Compile C Code : gcc filename.c
-gcc helloworld.c
+# #Compile C Code : gcc filename.c
+# gcc helloworld.c
 
-#Run the code
-./a.out
+# #Run the code
+# ./a.out
 
-#Compile C Code : gcc filename.c
-gcc fibonacci.c
+# #Compile C Code : gcc filename.c
+# gcc fibonacci.c
 
-#Run the code
-./a.out
+# #Run the code
+# ./a.out
+
+sudo python get-pip.py
+sudo python -m pip install ansible
