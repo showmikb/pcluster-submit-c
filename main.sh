0ff1adad6ac8bce70a54e5354442836083a29ab0
Showmik Bose <officialshowmik@gmail.com>
2021-08-12 11:19:46 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
 #Append your authentication token if it is a private repo : git clone https://token@github.com/showmikb/pcluster-submit-jobs.git
-DIR="pcluster-submit-c"
-if [ -d "$DIR" ]; then
-  # Take action if $DIR exists. #
-  echo "${DIR} Present"
-  cd $DIR
-  git pull https://github.com/showmikb/$DIR.git
+# DIR="pcluster-submit-c"
+# if [ -d "$DIR" ]; then
+#   # Take action if $DIR exists. #
+#   echo "${DIR} Present"
+#   cd $DIR
+#   git pull https://github.com/showmikb/$DIR.git
 
-else 
-  echo "Directory $DIR Not Present"
-  git clone https://github.com/showmikb/$DIR.git
-  cd $DIR
-fi
+# else 
+#   echo "Directory $DIR Not Present"
+#   git clone https://github.com/showmikb/$DIR.git
+#   cd $DIR
+# fi
 
-if rpm -qa autoconf
-then
-    echo "Proceeding as required installations are present"
-else
-    yum install autoconf
-    yum install automake
-fi
+# if rpm -qa autoconf
+# then
+#     echo "Proceeding as required installations are present"
+# else
+#     yum install autoconf
+#     yum install automake
+# fi
 
-aclocal
-autoconf
-automake --add-missing
-./configure
-make distcheck
+# aclocal
+# autoconf
+# automake --add-missing
+# ./configure
+# make distcheck
+
+aws s3 cp s3://pcluster-submit-c-artifact/fibonacci-0.1.tar.gz .
+
 
 echo "Submitting Jobs"
 qsub sampleshell.sh
